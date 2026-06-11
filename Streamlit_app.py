de05837ff01cc13814fcccacc4b2d6e258af726d
rayankhazi-kipi <rayan.m.khazi@kipi.ai>
2026-06-11 20:21:19 +0530

    Update Streamlit_app.py
    Conneced changes needed bc of move from is to SniS

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages.
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 
@@ -17,7 +17,9 @@ st.write(
 # st.write("The name on your smoothie will be ", name_on_order)
 
 
-session = get_active_session()
+# session = get_active_session()
+cnx = st.connection("snwflake")
+session = cn.session()
 
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
